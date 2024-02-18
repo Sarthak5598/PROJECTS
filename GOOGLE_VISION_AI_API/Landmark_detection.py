d607d2ce14ac318b7ac078f088ae9ade9052250b
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-02-18 16:13:30 +0530

    LANDMARK

--- GOOGLE_VISION_AI_API/Landmark_detection.py
+++ GOOGLE_VISION_AI_API/Landmark_detection.py
@@ -4,8 +4,10 @@ from google_vision_ai import VisionAI
 from google_vision_ai import prepare_image_local,prepare_image_web,draw_boundary,draw_boundary_normalized
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'D:\C C++\PROJECTS\GOOGLE_VISION_AI_API\client_google_api.json'
 client = vision.ImageAnnotatorClient()
-image_path="D:\C C++\PROJECTS\GOOGLE_VISION_AI_API\images\download (4).jpg"
+image_path="D:\C C++\PROJECTS\GOOGLE_VISION_AI_API\images\download (5).jpg"
 image= prepare_image_local(image_path)
 va = VisionAI(client,image)
 landmarks = va.landmark_detection()
-print(landmarks)
+for landmark in landmarks:
+    print(landmark.description)
+    print(landmark.score)
