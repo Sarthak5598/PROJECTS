aa0912d36467b7b9a70d1864a438bc2d3466dabc
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-02-18 12:42:02 +0530

    gitignore

--- GOOGLE_VISION_AI_API/basic_usage.py
+++ GOOGLE_VISION_AI_API/basic_usage.py
@@ -3,9 +3,8 @@ import os
 from google.cloud import vision
 from google_vision_ai import VisionAI
 from google_vision_ai import prepare_image_local,prepare_image_web,draw_boundary,draw_boundary_normalized
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'client_google_api.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'D:\C C++\PROJECTS\GOOGLE_VISION_AI_API\client_google_api.json'
 client = vision.ImageAnnotatorClient()
-
 image_path = 'D:\C C++\PROJECTS\GOOGLE_VISION_AI_API\images\download (3).jpg'
 with io.open(image_path, 'rb') as image_file:
     content = image_file.read()
